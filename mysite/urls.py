9eee5370352e53cf93d1f985c54b70008e541e45
Youjung <handgonpo@naver.com>
2026-01-16 16:12:13 +0900

    [feat] part1~part3

---
+++ mysite/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # path("", include("polls.urls")),
+    # http://127.0.0.1:8000
+    path("polls/", include("polls.urls")),
+    # http://127.0.0.1:8000/polls/
+]
