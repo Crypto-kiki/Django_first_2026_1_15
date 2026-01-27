ba385b8e1ff95c4de075bba8cc4cf5ed276a9743
Crypto-kiki <ejo2123@naver.com>
2026-01-27 17:42:26 +0900

    practice_view

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from . import practice_views
 
 app_name = "polls"
 
@@ -31,4 +32,18 @@ urlpatterns = [
     # http://127.0.0.1:8000/polls/1/delete/
     # 제너릭에 DeleteView 상속받아서 클래스 글 삭제를 구현
     # url polls:question_delete 탬플릿(html)에서 링크 형태로 호출
+
+    # ✅ 연습용 practice_views (브라우저 확인)
+    path("practice/1/", practice_views.practice_1, name="practice_1"),
+    path("practice/2/", practice_views.practice_2, name="practice_2"),
+    path("practice/3/", practice_views.practice_3, name="practice_3"),
+    path("practice/5/", practice_views.practice_5, name="practice_5"),
+    path("practice/6/", practice_views.practice_6, name="practice_6"),
+
+    # ✅ 연습용 (플랫폼 확인: JSON)
+    path("practice/api/1/", practice_views.practice_api_1, name="practice_api_1"),
+    path("practice/api/2/", practice_views.practice_api_2, name="practice_api_2"),
+    path("practice/api/3/", practice_views.practice_api_3, name="practice_api_3"),
+    path("practice/api/5/", practice_views.practice_api_5, name="practice_api_5"),
+    path("practice/api/6/", practice_views.practice_api_6, name="practice_api_6"),
 ]
